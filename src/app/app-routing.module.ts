1f5d15a2cde55a9e56efb36f3345696be163bf67
kevinlysander27 <64511709+kevinlysander27@users.noreply.github.com>
2023-10-10 19:50:25 +0700

    minggu 6

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { AdminComponent } from './admin/admin.component';
+
 
 const routes: Routes = [
-  {path: "dashboard", component: DashboardComponent},
+  {path: "admin", component: AdminComponent},
   {path: "login", component: LoginComponent},
   {path: "register", component: RegisterComponent},
   {path: "", redirectTo: "login", pathMatch:"full"},
@@ -16,3 +17,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+ 
