25a96128287aaaa0c66ee003b4b0e51f78cf8caf
kevinlysander27 <64511709+kevinlysander27@users.noreply.github.com>
2023-10-17 19:48:40 +0700

    minggu 7

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { Dashboard2Component } from './dashboard2/dashboard2.component';
+import { Dashboard3Component } from './dashboard3/dashboard3.component';
 import { AdminComponent } from './admin/admin.component';
 
 
@@ -9,7 +12,11 @@ const routes: Routes = [
   {path: "admin", component: AdminComponent},
   {path: "login", component: LoginComponent},
   {path: "register", component: RegisterComponent},
+  {path: "dashboard", component: DashboardComponent},
+  {path: "dashboard2", component: Dashboard2Component},
+  {path: "dashboard3", component: Dashboard3Component},
   {path: "", redirectTo: "login", pathMatch:"full"},
+
 ];
 
 @NgModule({
